79b3d0a7a4c59a30e89b1ab9fc287c6bcc4562c5
Kasjan12 <103021098+Kasjan12@users.noreply.github.com>
2023-06-02 19:34:19 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,27 @@
 // Created by zomer on 19/03/2023.
 //
 #include "Window.h"
+#include "funkcje.h"
 int main()
 {
     int a;
     std::cin >> a;
     switch(a) {
         case 1:
+            {
             Window okno;
             okno.Action_Loop(500);
+            break;
+            }
+       case 2:
+            {
+              odbijanie();
+              break;
+            }
+       case 3:
+            {
+              spadanie_pionowe();
+              break;
+            }
     }
 }
